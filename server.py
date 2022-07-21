a916fb7b41be28c70947c482a8e399fc0c7e9793
Gumbo64 <chickennuggetcrazykid@gmail.com>
2022-07-21 22:49:47 +1000

    spam testing

--- server.py
+++ server.py
@@ -4,6 +4,7 @@ from subprocess import Popen
 import platform
 from os import system
 import urllib
+from random import random,getrandbits
 
 import logging
 
@@ -11,7 +12,7 @@ app = flask.Flask(__name__)
 logging.getLogger('werkzeug').disabled = True
 
 window_vars = []
-
+window_cooldowns = []
 @app.route('/<allvar>', methods=['GET'])
 def my_view_func(allvar):
     vars = allvar.split("|")
@@ -24,7 +25,15 @@ def my_view_func(allvar):
     # system('clear')
     # for i in window_vars:
     #     print(i) 
-    action = {'x': 200, 'y':30,'fire':True}
+       
+    fire = window_cooldowns[int(valdict["gameid"])] > 15
+    if fire:
+        window_cooldowns[int(valdict["gameid"])] = 0
+    else:
+        window_cooldowns[int(valdict["gameid"])]+=1
+
+
+    action = {'x': random()-0.5, 'y':random()-0.5,'fire':fire}
     response = urllib.parse.urlencode(action)
     return response
 
@@ -32,7 +41,7 @@ if __name__ == '__main__':
     my_os = platform.system()
     # config
     c = {
-        "N_WINDOWS":4,
+        "N_WINDOWS":50,
         "WIDTH":100,
         "HEIGHT":100,
     }
@@ -40,10 +49,12 @@ if __name__ == '__main__':
     if my_os == "Linux":
         for i in range(c["N_WINDOWS"]):
             window_vars.append({})
+            window_cooldowns.append(0)
             Popen(['./ruffle', "spidermanmodded.swf","--width",str(c["WIDTH"]), "--height",str(c["HEIGHT"]), "-P","gameid="+str(i)])
     else:
         for i in range(c["N_WINDOWS"]):
             window_vars.append({})
+            window_cooldowns.append(0)
             Popen(['./ruffle.exe', "spidermanmodded.swf","--width",str(c["WIDTH"]), "--height",str(c["HEIGHT"]), "-P","gameid="+str(i)])
     app.run(port=8000)
     
