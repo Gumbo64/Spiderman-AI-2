581320967fc0ea8012adc3e13660eab928c5bacf
Rory Coughlan <35547211+Gumbo64@users.noreply.github.com>
2022-07-21 12:06:58 +1000

    a

--- server.py
+++ server.py
@@ -1,7 +1,11 @@
 import flask
 from flask import request
+from subprocess import Popen
+from os import system
+
+
 app = flask.Flask(__name__)
- 
+window_vars = []
 
 @app.route('/<allvar>', methods=['GET'])
 def my_view_func(allvar):
@@ -11,12 +15,18 @@ def my_view_func(allvar):
     valdict = {}
     for i in range(len(names)):
         valdict[names[i]] = float(values[i])
-
-    print(valdict) 
+    window_vars[int(valdict["gameid"])] = valdict
+    system('clear')
+    for i in window_vars:
+        print(i) 
     return "swag"
 
 if __name__ == '__main__':
+    N_WINDOWS = 4
+    for i in range(N_WINDOWS):
+        print(i)
+        window_vars.append({})
+        Popen(['./ruffle', "spidermanmodded.swf","--width","1", "--height","1", "-P","gameid="+str(i)])
  
-    # run() method of Flask class runs the application
-    # on the local development server.
     app.run(port=8000)
+    
