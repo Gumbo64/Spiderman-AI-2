ba4ddebe3fa38e34fd4137b69db7dc8010f546a4
Gumbo64 <chickennuggetcrazykid@gmail.com>
2022-07-28 23:29:09 +1000

    HYPE

--- socketman.py
+++ socketman.py
@@ -71,7 +71,9 @@ sock.settimeout(None)
 conn, addr = sock.accept()
 
 
-# start_time = time.time()
+start_time = time.time()
+
+
 
 if __name__ == '__main__':
     while True:
@@ -81,7 +83,10 @@ if __name__ == '__main__':
 
 
         vars = parse_vars(data)
+        
+
         print(vars)
+        # print([vars["w1x"],vars["w1y"],vars["w1vx"],vars["w1vy"],vars["w2x"],vars["w2y"],vars["w2vx"],vars["w2vy"]])
         action = {'x': random()-0.5, 'y':random()-0.5,'fire':get_fire(vars["gameid"])}
 
 
@@ -92,6 +97,7 @@ if __name__ == '__main__':
         encoded_action = urllib.parse.urlencode(action)
         response = "HTTP/1.1 " + str(Response(text=encoded_action))
         conn.send(response.encode('utf-8'))
-        # print("FPS: ", 1.0 / (time.time() - start_time))
-        # start_time = time.time()
+        # time.sleep(1)
+        # print("FPS: ", 1.0 / max(time.time() - start_time,1e-10))
+        start_time = time.time()
     sock.close()
--- spidermanENV.py
+++ spidermanENV.py
@@ -1,25 +1,17 @@
 
 # environment
-from click import option
-import cv2
 import numpy as np
 from matplotlib import pyplot as plt
-import time
 from gym import Env
-from gym.spaces import Box, Discrete
+from gym.spaces import Box
 
 import socket
-import time
 import urllib.parse
 from random import random
 from subprocess import Popen
 import platform
-from collections import defaultdict
 from webob import Response
-import selectors
 
-
-import os 
 
 
 
@@ -30,12 +22,14 @@ class Spiderman_ENV(Env):
 		# Setup spaces
 		
 		self.input_dims = 32
-		self.observation_space = Box(low=0, high=1, shape=self.input_dims, dtype=np.float32)
+		self.observation_space = Box(low=0, high=1, shape=(self.input_dims,), dtype=np.float64)
 
-		# 20 grid positions + none
-		self.action_space = Box( np.array([0,0]), np.array([+1,+1]))
+		# first 2 inputs effectively a normalised vector output
+		# when u click in the game it calculates the angle and nothing else so this range is all you need
+		# 3rd input is whether the agent wants to fire a web or not, if it the value is above a threshold it will fire
+		# this is important because spamming shots causes the tip of the webs to fly away, meaning they won't attach to anything and spiderman dies
 
-		self.last_score=295
+		self.action_space = Box( np.array([-1,-1,-1]), np.array([+1,+1,+1]))
 			
 		self.c = {
 			# "N_WINDOWS":10,
@@ -55,15 +49,32 @@ class Spiderman_ENV(Env):
 		sock.setblocking(False)
 		sock.settimeout(None)
 		self.conn, addr = sock.accept()
+	def open_windows(self):
+		ruffle_launchers = {
+			"Linux":"./ruffle",
+			"Windows":"./ruffle.exe"
+		}
+
+		my_os = platform.system()
+		
+
+		# for i in range(c["N_WINDOWS"]):
+		Popen([ruffle_launchers[my_os], "spidermanmodded.swf","--width",str(self.c["WIDTH"]), "--height",str(self.c["HEIGHT"]), "-P","gameid="+str(0)])
 
 
-
-	def step(self, action):
+	def step(self, action_array):
 		data = self.conn.recv(1024)
 		self.vars = self.parse_vars(data)
-		
-		done = self.vars["done"]
+		action = {'x': action_array[0], 'y':action_array[1],'fire':action_array[2] > 0}
 
+		encoded_action = urllib.parse.urlencode(action)
+		response = "HTTP/1.1 " + str(Response(text=encoded_action))
+		self.conn.send(response.encode('utf-8'))
+
+
+		observation = self.get_observation()
+		done = self.get_done()
+		reward = self.get_reward()
 
 		
 		
@@ -78,8 +89,8 @@ class Spiderman_ENV(Env):
 		vars = str_data.split("|")
 		names = vars[:-2:2]
 		values = vars[1:-2:2]
-
 		raycasts = list(map(float,vars[-1].split(",")))
+
 		valdict = {"raycasts":raycasts}
 		for i in range(len(names)):
 			valdict[names[i]] = float(values[i])
@@ -87,28 +98,47 @@ class Spiderman_ENV(Env):
 		return valdict
 	
 	def reset(self):
+		data = self.conn.recv(1024)
+		self.vars = self.parse_vars(data)
+		action = {'x':0, 'y':0,'fire':False}
+
+		encoded_action = urllib.parse.urlencode(action)
+		response = "HTTP/1.1 " + str(Response(text=encoded_action))
+		self.conn.send(response.encode('utf-8'))
+
+
 		return self.get_observation()
 
 
-
 	def get_observation(self):
-		# get an updated image of the game
+		v = self.vars
+		observation = []
+		observation += v["raycasts"] # 20
 		
-		return np.resize(blackAndWhiteImage, self.input_dims)
+		rel_coord_array = [v["w1x"],v["w2x"],v["w1y"],v["w2y"]] #4
+		# clamp and normalise coords
+		rel_coord_array = list(map(lambda n: ((sorted((-600,n, 600))[1]/600 + 1)/2), rel_coord_array))
+
+		vel_array = [v["vx"],v["vy"],v["w1vx"],v["w2vx"],v["w1vy"],v["w2vy"]] #6
+		vel_array = list(map(lambda n: ((sorted((-200,n, 200))[1]/200 + 1)/2), vel_array))
+
+		# 600 is a bit more than the screen height
+		y_coord =(sorted((-600,v["y"], 600))[1]/600 + 1)/2  # should be between 0 and 1
+		hand = v["hand"]
+
+
+		observation += vel_array
+		observation += rel_coord_array
+		observation.append(y_coord)
+		observation.append(hand)
+
+		final = np.array(observation)
+		return final
 
 
 	def get_reward(self):
-		
+		return self.vars["reward"]
 
-		reward = score - self.last_score
-		self.last_score=score
-		# print(reward)
-		return reward
-	# def get_score(self):
-	# 	return self.lastscore
 
-	def get_done(self,observation):
-		# print(np.sum(observation)/255)
-		if np.sum(observation)/255 > 6000:
-			return True
-		return False
+	def get_done(self):
+		return self.vars["done"] != 0
